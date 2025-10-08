944c8c398796d757e910d89090cc4ca8506a7925
Pranav Verma <pranav@verma.net.in>
2025-10-09 00:50:44 +0530

    Update: Implement A20 gate enablement, GDT loading, and protected mode entry in bootloader

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -26,6 +26,24 @@ start:
 
     call load_kernel
 
+    ; Enable A20 (fast method)
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    ; Load GDT
+    lgdt [gdt_descriptor]
+
+    ; Enter protected mode
+    mov si, msg_protected
+    call print_string
+
+    cli
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:protected_mode_start
+
 print_string:
     pusha
 .loop:
@@ -38,11 +56,6 @@ print_string:
 .done:
     popa
     ret
-
-halt:
-    cli
-    hlt
-    jmp halt
 
 load_kernel:
     mov si, msg_loading
@@ -77,6 +90,71 @@ disk_error:
     mov si, msg_error
     call print_string
     jmp $
+
+[BITS 32]
+
+protected_mode_start:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x90000
+
+    mov edi, 0xB8000
+    mov ecx, 2000
+    mov ax, 0x0F20
+    rep stosw
+
+    mov edi, 0xB8000
+    mov esi, pm_msg
+.loop:
+    lodsb
+    test al, al
+    js .done
+    mov ah, 0x0F
+    stosw
+    jmp .loop
+.done:
+    mov eax, KERNEL_OFFSET
+    call eax
+
+halt:
+    cli
+    hlt
+    jmp halt
+
+
+[BITS 16]
+align 4
+gdt_start:
+    dq 0x0
+
+gdt_code:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+
+gdt_data:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 ; Data
 boot_drive:     db 0
