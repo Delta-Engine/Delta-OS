a5998cf81e258796d198cd938fc882e93bab5f5e
Pranav Verma <pranav@verma.net.in>
2025-10-09 00:46:22 +0530

    Update: Implement load_kernel function for bootloader and enhance error handling

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -5,6 +5,9 @@
 [BITS 16]
 [ORG 0x7C00] ; hell nah man
 
+KERNEL_OFFSET equ 0x1000
+KERNEL_SEGMENT equ 0x0000
+
 ; Start Dir
 start:
     cli
@@ -20,6 +23,8 @@ start:
     ; welcome message
     mov si, msg_welcome
     call print_string
+
+    call load_kernel
 
 print_string:
     pusha
@@ -38,6 +43,40 @@ halt:
     cli
     hlt
     jmp halt
+
+load_kernel:
+    mov si, msg_loading
+    call print_string
+
+    mov ah, 0x02    ; Read Sectors
+    mov al, 32      ; Number of sectors
+    mov ch, 0       ; Cylinder No. 0
+    mov cl, 2       ; Sector 2
+    mov dh, 0       ; Head 0
+    mov dl, [boot_drive]
+    mov bx, KERNEL_SEGMENT
+    mov es, bx
+    mov bx, KERNEL_OFFSET
+    int 0x13
+    jc disk_error
+
+    ; For Verification
+    push es
+    mov ax, KERNEL_SEGMENT
+    mov es, ax
+    mov ax, [es:KERNEL_OFFSET]
+    pop es
+    test ax, ax
+    jz disk_error
+
+    mov si, msg_ok
+    call print_string
+    ret
+
+disk_error:
+    mov si, msg_error
+    call print_string
+    jmp $
 
 ; Data
 boot_drive:     db 0
